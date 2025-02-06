bdae697b4bc728d346f21962b58a0a38e19a978d
suplancee <Lpaangcceu05@gamil.com>
2025-02-06 13:19:44 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Lance Chincuanco</h1>
+      <div className="box">
+        <p className="text">CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
